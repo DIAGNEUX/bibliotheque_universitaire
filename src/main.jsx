3196775e58000fec12725a01755cebb957b691f1
insta <aliouned007@gmail.com>
2025-04-14 21:35:40 +0200

    deploy

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter> {/* Celui-ci enveloppe toute l'application */}
+    <BrowserRouter basename="/bibliotheque_universitaire/"> 
       <App />
     </BrowserRouter>
   </StrictMode>
